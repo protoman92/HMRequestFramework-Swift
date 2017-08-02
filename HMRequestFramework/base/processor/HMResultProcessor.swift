c95409a1b88ffea7c741a9a9fbd6230e7cd0d99b
Hai Pham <swiften.svc@gmail.com>
2017-08-02 23:25:35 +0800

    Added eqProcessor() for HMResultProcessors

--- HMRequestFramework/base/processor/HMResultProcessor.swift
+++ HMRequestFramework/base/processor/HMResultProcessor.swift
@@ -17,35 +17,3 @@ public typealias HMEQResultProcessor<Val> = HMResultProcessor<Val,Val>
 
 public typealias HMProtocolResultProcessor<Val: HMProtocolConvertibleType> =
     HMResultProcessor<Val,Val.PTCType>
-
-/// Common result processors.
-
-public final class HMResultProcessors {
-    
-    /// Convenience method to process the result from some request into a
-    /// specified type.
-    ///
-    /// - Parameters:
-    ///   - previous: A Try instance that contains the request result.
-    ///   - processor: Processor function to convert said result into some type.
-    /// - Returns: An Observable instance.
-    /// - Throws: Exception if the processing fails.
-    public static func processResultFn<Val,Res>(
-        _ previous: Try<Val>,
-        _ processor: @escaping HMResultProcessor<Val,Res>) throws
-        -> Observable<Try<Res>>
-    {
-        return previous.rx.get()
-            .flatMap(processor)
-            .catchErrorJustReturn(Try.failure)
-    }
-    
-    /// Get a result processor that does no transformation on the request result.
-    ///
-    /// - Returns: A HMResultProcessor instance.
-    public static func eqProcessor<Val>() -> HMEQResultProcessor<Val> {
-        return {Observable.just(Try.success($0))}
-    }
-    
-    private init() {}
-}
