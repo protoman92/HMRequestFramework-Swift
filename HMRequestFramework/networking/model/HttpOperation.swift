7a9a6228e66eede282f383f2fd131b80596f6406
Daniel Tjuatja <daniel.tjuatja@gmail.com>
2017-09-15 11:46:43 +0800

    Add patch to http operation

--- HMRequestFramework/networking/model/HttpOperation.swift
+++ HMRequestFramework/networking/model/HttpOperation.swift
@@ -11,12 +11,13 @@ import SwiftUtilities
 public enum HttpOperation: EnumerableType {
     case get
     case post
+    case patch
     case head
     case put
     case upload
     
     public static func allValues() -> [HttpOperation] {
-        return [.get, .post, .head, .put, .upload]
+        return [.get, .post, .patch, .head, .put, .upload]
     }
     
     public func method() -> String {
@@ -27,11 +28,15 @@ public enum HttpOperation: EnumerableType {
         case .post, .upload:
             return "POST"
             
+        case .patch:
+            return "PATCH"
+            
         case .head:
             return "HEAD"
             
         case .put:
             return "PUT"
+
         }
     }
     
@@ -40,7 +45,7 @@ public enum HttpOperation: EnumerableType {
     /// - Returns: A Bool value.
     public func requiresBody() -> Bool {
         switch self {
-        case .post, .put, .upload:
+        case .post, .put, .upload, .patch:
             return true;
             
         default:
